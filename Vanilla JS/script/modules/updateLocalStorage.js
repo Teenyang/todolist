8ec158c369722176f8433f7fa45919a09f3dd31d
Teen Yang <chopcostop@gmail.com>
2021-06-26 18:03:12 +0800

    change render newTask by js, not show in HTML at first

--- Vanilla JS/script/modules/updateLocalStorage.js
+++ Vanilla JS/script/modules/updateLocalStorage.js
@@ -1,31 +1,87 @@
 import { compareDaysAgo, separateDateFormatWithSlash, captureDeadlineCalendarFormat } from '../modules/convertDateFormat.js'
 
+const main = document.querySelector('main');
+const taskList = document.querySelector('.task_list');
+
+function renderNewTask() {
+  const newTask = document.createElement('form');
+  main.insertBefore(newTask, taskList);
+
+  newTask.outerHTML = `
+    <form id="task-edit" class="task new_task editing" autocomplete="off" name="task-list">
+      <section class="task_header">
+        <div class="title_group">
+          <input type="checkbox" class="done_task" id="doneTask">
+          <label for="doneTask"><i class="far fa-check"></i></label>
+          <input type="text" class="task_title" name="task-title" placeholder="Type Something Here..." required>
+          <div class="marker_group">
+            <i class="far fa-star star_task general_icon"></i>
+            <i class="fas fa-star star_task star_icon"></i>
+            <i class="far fa-pen edit_task general_icon"></i>
+            <i class="fas fa-pen edit_task pen_icon"></i>
+          </div>
+        </div>
+      </section>
+      <div class="task_content">
+        <section class="task_body">
+          <div class="edit_item deadline">
+            <label><i class="far fa-calendar-alt fa-fw edit_icon"></i>Deadline</label>
+            <div class="edit_content">
+              <input id="date" type="text" class="deadline_date" placeholder="yyyy/mm/dd" name="deadline-date">
+              <input id="time" type="text" class="deadline_time" placeholder="hh:mm" name="deadline-time">
+            </div>
+          </div>
+          <div class="edit_item file">
+            <label><i class="far fa-file fa-fw edit_icon"></i>File</label>
+            <div class="edit_content">
+              <div class="file_data">
+                <!-- <a class="download_file"> -->
+                <span class="upload_fileName"></span>
+                <!-- </a> -->
+                <p class="upload_days_ago">
+                  <span class="upload_dateSlash"></span>
+                </p>
+                <span class="upload_dateMillisecond"></span>
+              </div>
+              <input id="upload" type="file" class="upload_file" name="file-upload">
+              <label for="upload"><i class="fal fa-plus fa-fw"></i></label>
+            </div>
+          </div>
+          <div class="edit_item comment">
+            <label><i class="far fa-comment-dots fa-fw edit_icon"></i>Comment</label>
+            <div class="edit_content">
+              <textarea class="edit_comment" name="comment" placeholder="Type your memo here…" readonly></textarea>
+            </div>
+          </div>
+        </section>
+        <section class="task_footer">
+          <button type="reset" class="task_cancel"><i class="fal fa-times"></i>Cancel</button>
+          <button type="submit" class="task_editing"><i class="fal fa-plus"></i>Add Task</button>
+        </section>
+      </div>
+    </form>
+  `
+}
+
+
 //* 將存進localStorage的task data透過array.map()更新至taskList區域，逐一匯出task內容
 // function renderTaskList(tasksDataArray, taskList) {
 function renderTaskList() {
-  const taskList = document.querySelector('.task_list');
   const tasksDataArray = JSON.parse(localStorage.getItem('lists')) || [];
-
   taskList.innerHTML = tasksDataArray.map((task, index) => {
     return `
       <form data-index="${index}" id="task-edit" autocomplete="off" name="task-list" class="task ${task.done ? 'completed' : ''} ${task.star ? 'star' : ''} ${(task.deadlineDate !== '') || (task.file !== '') || (task.comment !== '') ? 'progress' : ''} drag">
         <section class="task_header">
           <div class="title_group">
-            <input type="checkbox" data-done="${index}" data-index="${index}" class="done_task" id="doneTask${index}" ${task.done ? 'checked' : ''}>
+            <input type="checkbox" class="done_task" id="doneTask${index}" ${task.done ? 'checked' : ''}>
             <label for="doneTask${index}"><i class="far fa-check"></i></label>
-            <input data-title="${index}" type="text" class="task_title" name="task-title" placeholder="Type Something Here..." required value="${task.title}">
+            <input type="text" class="task_title" name="task-title" placeholder="Type Something Here..." required value="${task.title}">
             <div class="marker_group">
-              <input type="checkbox" data-star="${index}" data-index="${index}" class="star_task" id="markerStar${index}" ${task.star ? 'checked' : ''}>
-              <label for="markerStar${index}">
-                <i class="far fa-star general"></i>
-                <i class="fas fa-star star"></i>
-              </label>
-              <input type="checkbox" data-edit="${index}" class="edit_task" id="markerPen${index}">
-              <label for="markerPen${index}">
-                <i class="far fa-pen general"></i>
-                <i class="fas fa-pen edit"></i>
-              </label>
-              <i data-delete="${index}" class="far fa-trash-alt general delete_task"></i>
+              <i class="far fa-star star_task general_icon"></i>
+              <i class="fas fa-star star_task star_icon"></i>
+              <i class="far fa-pen edit_task general_icon"></i>
+              <i class="fas fa-pen edit_task pen_icon"></i>
+              <i class="far fa-trash-alt delete_task general_icon"></i>
             </div>
           </div>
           <div class="info_group">
@@ -53,7 +109,7 @@ function renderTaskList() {
                   </p>
                   <span class="upload_dateMillisecond">${(task.file !== '') ? task.fileUpload : ''}</span>
                 </div>
-                <input data-upload="${index}" id="upload${index}" type="file" class="upload_file" name="file-upload" value="${task.file}">
+                <input id="upload${index}" type="file" class="upload_file" name="file-upload" value="${task.file}">
                 <label for="upload${index}"><i class="fal fa-plus fa-fw"></i></label>
               </div>
             </div>
@@ -87,4 +143,4 @@ function DeleteLocalStorage() {
 }
 clearStorageButton.addEventListener('click', DeleteLocalStorage);
 
-export { renderTaskList, saveToLocalStorage, DeleteLocalStorage }
+export { renderNewTask, renderTaskList, saveToLocalStorage, DeleteLocalStorage }
